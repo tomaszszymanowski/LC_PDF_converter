8f884ffad368b1f0742219b0f351d99e7278ea76
Tomasz Szymanowski <tomasz.szymanowski@gmail.com>
2024-11-17 23:52:12 +0100

    #1 add choose txt file

--- gui.py
+++ gui.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from tkinter import filedialog, Menu
-import extraxt_from_pdf
+from tkinter import Text, filedialog, Menu
+
 
 class FileChooserApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("File Chooser with Menu Bar")
+        self.root.title("LC Pdf Converter")
         self.selected_file = None
         self.saved_file = None
 
@@ -21,20 +21,31 @@ class FileChooserApp:
         convert_menu = Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Convert", menu=convert_menu)
 
+        # dodanie menu display
+        display_menu = Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Display", menu=display_menu)
+
         # Opcje w menu "File"
         file_menu.add_command(label="Open File", command=self.open_file)
         file_menu.add_command(label="Save File", command=self.save_file)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.root.quit)
 
+        # opcje w menu "Display"
+        display_menu.add_command(label="Display results", command=self.open_file)
+
+        # text field
+        text_field = Text(self.root, width=78, height=20)
+        text_field.pack(pady=10)
+
+
     def open_file(self):
         self.selected_file = filedialog.askopenfilename(
             title="Select a file",
-            filetypes=(("PDF Files", "*.pdf"), ("All Files", "*.*"))
+            filetypes=(("PDF Files", "*.pdf"),("Txt Files", "*.txt"), ("All Files", "*.*"))
         )
         if self.selected_file:
             print(f"Selected file: {self.selected_file}")
-            extraxt_from_pdf.extraxt_data(self.selected_file)
 
     def save_file(self):
         self.saved_file = filedialog.asksaveasfilename(
@@ -44,9 +55,15 @@ class FileChooserApp:
         )
         if self.saved_file:
             print(f"File will be saved as: {self.saved_file}")
+    
+    def show_result():
+        
+        pass
+
 
 # Uruchomienie aplikacji
 root = tk.Tk()
 root.geometry("800x600")
 app = FileChooserApp(root)
+
 root.mainloop()
